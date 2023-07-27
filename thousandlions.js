f693c58fe2e37fc9a1b6469565c9774cce6b4763
Mazzy <Hatsunefanqwerty@gmail.com>
2023-07-27 16:59:43 -0500

    worked on stuff

--- thousandlions.js
+++ thousandlions.js
@@ -1,52 +1,47 @@
+// Enumerate your locations
+const LOCATIONS = {
+    HOME: 'home',
+    //...
+};
+
+// Initiate the state
 let state = {
     day: 1,
     hour: 0,
     energy: 100,
     eventSequence: [],
-    currentActivityIndex: -1
+    currentActivityIndex: -1,
+    location: LOCATIONS.HOME,
+    inventory: []
 };
 
-let activityIndex = 0;
+// Class
+class Item {
+    constructor(name, power = 0, repeatable = true) {
+        this.name = name;
+        this.power = power;
+        this.repeatable = repeatable;
+    }
+}
+
+// Define your events
+const EVENTS = [
+    { name: 'Find Wooden Sword', item: new Item('Wooden Sword', 10, false), rarity: 10, locations: [LOCATIONS.HOME], energy: 5 },
+    { name: 'Find Low Grade Pill', item: new Item('Low Grade Pill', 20, true), rarity: 20, locations: [LOCATIONS.HOME], energy: 5 },
+    //.. etc
+];
 
 function ranOutOfStamina(){
     alert('Ran out of stamina! Game over.');
     clearInterval(loop);
 };
 
-function incrementDay() {
-    incrementActivity();
-    if(state.currentActivityIndex == state.eventSequence.length){
-        state.hour = 0; 
-        state.currentActivityIndex = 0;
-        state.day++;
-        document.getElementById('dayCount').innerText = state.day;
-    }
-}
-
-function incrementHour() {
-    if(state.hour == 24) {
-        state.hour = 0;
-    } else {
-        state.hour++;
-    }
-    document.getElementById('hourCount').innerText = state.hour;
-}
-
-
-function manageEnergy(activity){
-    if(activity.name === 'rest'){
-        state.energy += 10; 
-    } else {
-        state.energy -= 10; 
-    }
-    document.getElementById('energyCount').innerText = state.energy;
-}
-
 function addActivity(activity) {
     activity.duration = activity.duration || 1; 
-    let newActivityIndex = state.eventSequence.push(activity) - 1;
-    let newActivityEl = createActivityElement(newActivityIndex, activity);
-    document.getElementById('sequenceContainer').appendChild(newActivityEl);
+    let newIndex = state.eventSequence.push(activity) - 1;
+    let activityDiv = document.getElementById('sequenceContainer');
+    let newActivityEl = createActivityElement(newIndex, activity);
+    activityDiv.appendChild(newActivityEl);
 }
 
 function removeActivity(index) {
@@ -68,74 +63,117 @@ function manageTime(index, operator) {
 
 function createActivityElement(index, activity) {
     let activityEl = document.createElement('div');
-    activityEl.id = "activity"+index;
+    activityEl.id = "activity" + index;
     activityEl.className = "activity";
     
     let markerEl = document.createElement('div');
-    markerEl.id = "marker"+index;   // Re-add this line
+    markerEl.id = "marker" + index;
     markerEl.className = "marker";
     markerEl.style.backgroundColor = 'transparent';
 
     activityEl.innerHTML = 
         activity.name + ' for ' + 
-        '<span id="activityDuration'+index+'">'+activity.duration+'</span>' + ' hours '+
-        '<button onclick="manageTime('+index+', \'-\')"> - </button>'+
-        '<button onclick="manageTime('+index+', \'+\')"> + </button>'+
-        '<button onclick="removeActivity('+index+')"> X </button>';
+        '<span id="activityDuration' + index + '">' + activity.duration + '</span>' + ' hours '+
+        '<button onclick="manageTime(' + index + ', \'-\')"> - </button>' +
+        '<button onclick="manageTime(' + index + ', \'+\')"> + </button>' +
+        '<button onclick="removeActivity(' + index + ')"> X </button>';
 
     activityEl.prepend(markerEl);
     return activityEl;
-};
-
-function markCurrentActivity() {
-    // Remove the marker from the previous activity
-    if(state.currentActivityIndex > 0) {
-        document.getElementById('marker'+(state.currentActivityIndex-1)).style.backgroundColor = "red";
-    }
-
-    // Mark the current activity
-    document.getElementById('marker'+state.currentActivityIndex).style.backgroundColor = "green";
 }
 
-function getAndUpdateCurrentActivity() {
-    if(state.energy <= 0){
+// Add manageEnergy returning true or false based on outcome (energy not fallen below 0)
+function manageEnergy(activity){
+    let spentEnergy = activity.energy || 10; 
+    if(state.energy < spentEnergy){
         ranOutOfStamina();
+        return false;
     }
-    
-    if(state.eventSequence[state.currentActivityIndex].duration === 0) {
-        incrementDay();
-        document.getElementById('currentEvent').innerText = state.eventSequence[state.currentActivityIndex].name;
-        markCurrentActivity();
+    if(activity.name === 'rest'){
+        state.energy += 10; 
+    } else {
+        state.energy -= spentEnergy; 
     }
-
-    manageEnergy(state.eventSequence[state.currentActivityIndex]);
-    state.eventSequence[state.currentActivityIndex].duration--;
+    document.getElementById('energyCount').innerText = state.energy;
+    return true;
 }
 
-function gameLoop() {
-    incrementHour();
-    getAndUpdateCurrentActivity();
-    markCurrentActivity();
+function lookAround() {
+    let possibleEvents = EVENTS.filter(event => event.locations.includes(state.location) && (!state.inventory.find(item => item.name === event.item.name) || event.item.repeatable));
+    possibleEvents.sort((a, b) => a.rarity - b.rarity);
+    let eventFound = null;
+    for(let event of possibleEvents){
+        if(Math.random() * 100 < event.rarity){
+            eventFound = event;
+            state.inventory.push(event.item);
+            break;
+        }
+    }
+    return eventFound;
+}
+
+function changeLocation(newLocation) {
+    state.location = newLocation;
+    document.getElementById('location').innerText = state.location;
+}
+
+function displayInventory() {
+    const inventoryContainer = document.getElementById('inventory');
+    inventoryContainer.innerHTML = state.inventory.map(item => item.name).join(', ');
+}
+
+function incrementHour() {
+    if(state.hour == 24) {
+        state.hour = 0;
+    } else {
+        state.hour++;
+    }
+    document.getElementById('hourCount').innerText = state.hour;
 }
 
 window.addEventListener('DOMContentLoaded', function(){
+    displayInventory();
+
+    const eventListContainer = document.getElementById('eventList');
+
     let restEventButton = document.createElement('button');
     restEventButton.innerText = 'Rest';
     restEventButton.addEventListener('click', function(){
-        addActivity({name: 'rest', duration: 1});
+        addActivity({name: 'Rest', duration: 1, energy: -10});
     });
-    document.getElementById('eventList').appendChild(restEventButton);
+    eventListContainer.appendChild(restEventButton);
 
     let lookAroundEventButton = document.createElement('button');
     lookAroundEventButton.innerText = 'Look Around';
     lookAroundEventButton.addEventListener('click', function(){
-        addActivity({name: 'look around', duration: 1});
+        let event = lookAround();
+        if(event) {
+            addActivity(event);
+        } else {
+            addActivity({name: 'Look Around', duration: 1, energy: 10});
+        }
     });
-    document.getElementById('eventList').appendChild(lookAroundEventButton);
+    eventListContainer.appendChild(lookAroundEventButton);
 });
 
-// Start the game
-let loop = setInterval(function() {
-    gameLoop();
-    activityIndex = state.currentActivityIndex;
-}, 1000);
+function gameLoop() {
+    incrementHour();
+    if(!manageEnergy(state.eventSequence[state.currentActivityIndex])) {
+        ranOutOfStamina();
+        return;
+    }
+ 
+    if(state.eventSequence[state.currentActivityIndex].duration === 0){
+        state.currentActivityIndex++;
+        if(state.currentActivityIndex >= state.eventSequence.length){
+            state.currentActivityIndex = 0;
+        }
+    }
+
+    displayInventory();
+    markCurrentActivity();
+
+    state.eventSequence[state.currentActivityIndex].duration--;
+}
+
+let loop = setInterval(gameLoop, 1000);
